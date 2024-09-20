58fbf3efbcc007f3bfd46c46a970f053a22809a1
A H Shuvro <ahshuvro77777@gmail.com>
2024-09-20 12:52:44 +0600

    Added some social link

--- src/components/AnimationCanvas.jsx
+++ src/components/AnimationCanvas.jsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import { textVariant2 } from "./animations";
+import { Link } from 'react-scroll';
 
 
 const AnimationCanvas = () => {
@@ -7,23 +8,35 @@ const AnimationCanvas = () => {
         <div className="relative overflow-hidden text-white w-full sm:w-1/2 sm:min-h-screen sm:h-screen flex items-center">
             <motion.div
                 variants={textVariant2()} initial="hidden" animate="show"
-                className="relative z-10 mx-auto max-w-7xl px-6 lg:px-8 text-center"
+                className="relative z-10 mx-auto max-w-7xl px-6 lg:px-8 flex flex-col items-center justify-center text-center"
             >
                 <h1 className="text-4xl sm:text-6xl font-bold text-white"
                 >Hi, I'm <span className="text-[#6C56EF]">Shuvro</span></h1>
                 <p className="mt-4 text-gray-300 text-lg sm:text-xl" >
                     A passionate web developer specializing in creating interactive and visually stunning web applications.
                 </p>
-                <a href="#contact">
-                    <motion.button
-                        className=" mt-6 px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded-lg"
-                        initial={{ scale: 1 }}
-                        whileHover={{ scale: 1.05 }}
-                        transition={{ type: 'spring', stiffness: 300 }}
-                    >
-                        Get in Touch
-                    </motion.button>
-                </a>
+                <div className='flex gap-4 '>
+                    <a href='https://github.com/AHShuvro' target='blank'>
+                        <motion.button
+                            className=" mt-6 px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded-lg"
+                            initial={{ scale: 1 }}
+                            whileHover={{ scale: 1.05 }}
+                            transition={{ type: 'spring', stiffness: 300 }}
+                        >
+                            GitHub
+                        </motion.button>
+                    </a>
+                    <a href='https://www.linkedin.com/in/ahshuvro77777/' target='blank'>
+                        <motion.button
+                            className=" mt-6 px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded-lg"
+                            initial={{ scale: 1 }}
+                            whileHover={{ scale: 1.05 }}
+                            transition={{ type: 'spring', stiffness: 300 }}
+                        >
+                            Linkedin
+                        </motion.button>
+                    </a>
+                </div>
             </motion.div>
         </div>
     );
